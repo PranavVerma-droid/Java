9867cd32f9b7f2261aa78338bb218bf228a8c542
Pranav Verma <pranav@verma.net.in>
2024-04-07 20:23:17 +0530

    Added main.java

---
+++ Java/main.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class main {
+
+    void deftype() {
+        return;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Thine");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Java
  ♨️ Java Code ♨️
+
+Java Learning Code.
